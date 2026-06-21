42e2939af25b991d2afeb0b402b47563cef98219
claudiokurath <claudio.kurath@gmail.com>
2026-06-21 07:22:24 +0100

    chore: swap signin banner to teal Midjourney image

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -90,7 +90,7 @@ function SignupForm() {
   const heroBanner = (
     <section className="relative w-full min-h-[50vh] flex items-end overflow-hidden">
       <div className="absolute inset-0">
-        <img src="/Images/banners/sign%20in%20banner.png" alt="Sign in banner" className="absolute inset-0 w-full h-full object-cover" />
+        <img src="/Images/home/u1886598145_neurodiversity_--ar_169_--raw_--sref_httpss.mj.ru_411dc7a7-ffa1-4b3a-b81e-ab0deecf3fd0_2.png" alt="Sign in banner" className="absolute inset-0 w-full h-full object-cover" />
         <div className="absolute inset-0 bg-gradient-to-r from-black/80 via-black/40 to-black/10" />
       </div>
       <div className="relative z-10 max-w-6xl mx-auto px-5 sm:px-8 md:px-12 py-16 md:py-20 w-full">
